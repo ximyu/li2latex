3e3badc89ceb97e82a6f5c2f24af66f71f976208
Ximing Yu <ximing.yu@gmail.com>
2012-04-12 01:12:19 -0700

    Implemented a basic API Test Console which allow you to either specify a field name or enter an API URL

--- src/main/scala/apitest.scala
+++ src/main/scala/apitest.scala
@@ -3,19 +3,72 @@ package li2latex
 import org.scribe.builder.api.LinkedInApi
 import org.scribe.builder.ServiceBuilder
 import org.scribe.oauth.OAuthService
-import org.scribe.model.Token
+import org.scribe.model.{Verifier, Verb, OAuthRequest, Token}
+import org.scribe.exceptions.OAuthException
 
-object APITest {
-  val builder = new ServiceBuilder
-  val service: OAuthService =
+object APITestConsole {
+  private lazy val builder = new ServiceBuilder
+  private lazy val service: OAuthService =
     builder provider (new LinkedInApi) apiKey (AppConfig.API_KEY) apiSecret (AppConfig.API_SECRET) build()
-  val accessToken = getAccessToken
+  private lazy val accessToken = getAccessToken
 
   val getByUrl: String => String =
-    url => ""
+    url => {
+      val token = accessToken.get
+      val req = new OAuthRequest(Verb.GET, url)
+      service.signRequest(token, req)
+      req.send.getBody
+    }
 
   val getByField: String => String =
-    field => ""
+    field => getByUrl(String.format(AppConfig.API_DATA_URL, field))
 
-  private def getAccessToken : Option[Token] = None
+  /*
+   * Run the API Test Console from here
+   */
+  def start {
+
+    def getInstruction: Unit = {
+      println("Choose how you would like to test the API:")
+      println("(Note that the first time you use it you may need to login to LinkedIn)")
+      println("[f] Field, [u] URL, [q] Quit")
+
+      val nextFunc = readLine.toLowerCase match {
+        case "f" => Some(getByField)
+        case "u" => Some(getByUrl)
+        case _   => None
+      }
+
+      nextFunc map { f =>
+        print("Enter your " + {if (f == getByField) "Field" else "URL"} + ": ")
+        f(readLine)
+      } foreach { resp =>
+        println("LinkedIn Response:")
+        println(resp)
+        getInstruction
+      }
+    }
+
+    accessToken match {
+      case Some(token) => getInstruction
+      case None => println("Cannot retrieve access token from LinkedIn! Sorry...")
+    }
+  }
+
+  private def getAccessToken : Option[Token] = {
+      // Get the Request Token
+      val reqToken = service.getRequestToken()
+      val authUrl = service.getAuthorizationUrl(reqToken)
+      println("Please open the following authorization URL:\n")
+      println(authUrl)
+      println("\nPaste your verifier here:")
+      val v = new Verifier(readLine)
+      // Get the Access Token
+      try {
+        val accessToken = service.getAccessToken(reqToken, v)
+        Some(accessToken)
+      } catch {
+        case e: OAuthException => None
+      }
+  }
 }
--- src/main/scala/appconfig.scala
+++ src/main/scala/appconfig.scala
@@ -3,4 +3,6 @@ package li2latex
 object AppConfig {
   val API_KEY = "p8fo989psm4n"
   val API_SECRET = "gdQ3X9ZqXpL1IjzY"
+
+  val API_DATA_URL = "http://api.linkedin.com/v1/people/~:(%s)"
 }
